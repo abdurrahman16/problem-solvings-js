a23a4ebbfb4da08ddb104bd0d4673997eb179b73
Mohammad Abdur Rahman <rahman.just16@gmail.com>
2021-01-19 22:52:40 +0600

    randomNumber

--- problem.js
+++ problem.js
@@ -1,15 +1,21 @@
-//swap using js
-let a=5;
-let b=6;
-console.log('a=',a,'b=',b);
-let c=a;
-a=b;
-b=c;
-console.log('a=',a,'b=',b);
-console.log('a=',a,'b=',b);
+// //swap using js
+// let a=5;
+// let b=6;
+// console.log('a=',a,'b=',b);
+// let c=a;
+// a=b;
+// b=c;
+// console.log('a=',a,'b=',b);
+// console.log('a=',a,'b=',b);
 
-let p=5;
-let q=6;
+// let p=5;
+// let q=6;
 
-[p,q] = [q,p];
-console.log(p,q);
+// [p,q] = [q,p];
+// console.log(p,q);
+
+//random number
+
+let randomNumber =  Math.random()*500;
+let result = Math.round(randomNumber);
+console.log(result);
