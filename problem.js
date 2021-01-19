cfea98794792c6c8c3af583828a5abf6c3aa9659
Mohammad Abdur Rahman <rahman.just16@gmail.com>
2021-01-19 23:10:05 +0600

    largest in array

--- problem.js
+++ problem.js
@@ -16,6 +16,38 @@
 
 //random number
 
-let randomNumber =  Math.random()*500;
-let result = Math.round(randomNumber);
-console.log(result);
+// let randomNumber =  Math.random()*500;
+// let result = Math.round(randomNumber);
+// console.log(result);
+
+// let a= 300;
+// let b= 200;
+// let c= 300
+
+// if (a>b && a>c){
+//     console.log('a big');
+// }
+// else if (b>a && b>c){
+//     console.log('b is big')
+// }
+// else {
+//     console.log('c is big');
+// }
+// js function
+// let a=100;
+// b=300;
+// c=400;
+// let max= Math.max(a,b,c);
+// console.log(max);
+
+let marks=[33,55,66,77,88,23,90];
+let max =marks[0];
+
+for (let i=0; i<marks.length; i++){
+    let check= marks[i];
+    if(check>max)
+    {
+        max=check;
+    }
+}
+console.log(max);
