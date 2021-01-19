ee2590e09ae44b523c2a83d42eb9c8b75eee133b
Mohammad Abdur Rahman <rahman.just16@gmail.com>
2021-01-19 22:40:16 +0600

    swapping

---
+++ problem.js
@@ -0,0 +1,15 @@
+//swap using js
+let a=5;
+let b=6;
+console.log('a=',a,'b=',b);
+let c=a;
+a=b;
+b=c;
+console.log('a=',a,'b=',b);
+console.log('a=',a,'b=',b);
+
+let p=5;
+let q=6;
+
+[p,q] = [q,p];
+console.log(p,q);
